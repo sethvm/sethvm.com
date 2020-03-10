b3425c54d21cbad4ac1f47948c09e271dc2d7194
Seth Morenos <sethvm64@gmail.com>
2020-03-09 20:10:47 -0400

    complete desktop sidebar migration

---
+++ src/SideBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './bootstrap.min.css';
+import './animate.min.css';
+import './style.css';
+
+import resumeLogo from './svg/resume-logo.svg';
+import linkedinLogo from './svg/linkedin-logo.svg';
+import behanceLogo from './svg/behance-logo.svg';
+import githubLogo from './svg/github-logo.svg';
+import contactLogo from './svg/contact-logo.svg';
+
+import resumeFile from './pdf/sethvm_resume.pdf';
+
+
+function SideBar() {
+  return (
+    <div className='sidebar'>
+        <div className='side_items animated fadeInLeft'>
+            <a href={resumeFile} target={'blank'}><img src={resumeLogo} alt={'Resume'} className='side_logo' /></a>
+            <p><br /></p>
+            <a href={'https://www.linkedin.com/in/sethvm'} target={'_blank'}><img src={linkedinLogo} alt={'linkedin.com/in/sethvm'} className='side_logo' /></a>
+            <p><br /></p>
+            <a href={'https://www.behance.net/sethvm/'} target={'_blank'}><img src={behanceLogo} alt={'behance.net/sethvm'} className='side_logo' /></a>
+            <p><br /></p>
+            <a href={'https://github.com/sethvm'} target={'_blank'}><img src={githubLogo} alt={'github.com/sethvm'} className='side_logo' /></a>
+            <p><br /></p>
+            <a href={'mailto:sethvm64@gmail.com?cc=svmoreno@uwaterloo.ca'} target={'_blank'}><img src={contactLogo} className='side_logo' /></a>
+        </div>
+    </div>
+  );
+}
+
+export default SideBar;
