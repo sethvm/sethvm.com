0e9cd0caec28a9920b361915aa521f292d21cc1a
Seth Morenos <sethvm64@gmail.com>
2020-03-15 14:17:19 -0400

    rewrite navbar

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 import './bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './animate.min.css';
-import './style.css';
-import './home.css';
+import './components/Header.css';
 
 import './fonts/Futura PT_Light.css';
 import './fonts/Futura PT_Book.css';
@@ -16,29 +15,29 @@ import resumeFile from './pdf/sethvm_resume.pdf';
 function Header() {
     return (
         <div className='header animated fadeIn'>
-            <div className='nav_banner navbar navbar-light d-print'>
-                    <a className='navbar_left navbar-brand' href={''}><strong>SETH MORENOS</strong></a>
-                    <button className='mobile_nav navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls="navbarNav" aria-label="Toggle navigation">
-                        <span>
-                            <div className="menu_bar"></div>
-                            <div className="menu_bar"></div>
-                            <div className="menu_bar"></div>
-                        </span>
-                    </button>
-                    <div id='navbarNav' className='collapse navbar-collapse'>
-                        <div className='navbar-nav'>
-                            <div className='nav_drop'>
-                                <a className="nav-item nav-link active" href={''}>About</a>
-                                <a className="nav-item nav-link active" href={resumeFile}>Resume</a>
-                                <a className="nav-item nav-link active" href={'https://www.linkedin.com/in/sethvm/'}>LinkedIn</a>
-                                <a className="nav-item nav-link active" href={'https://www.behance.net/sethvm/'}>Behance</a>
-                                <a className="nav-item nav-link active" href={'https://github.com/sethvm'}>GitHub</a>
-                                <a className="nav-item nav-link active" href={'mailto:sethvm64@gmail.com?cc=svmoreno@uwaterloo.ca'}>Conact Me</a>
-                            </div>
-                        </div>
-                    </div>
-                <a className='desktop_nav navbar-brand' href={''}>ABOUT</a>
-            </div>
+            <Navbar className='nav_banner' bg='transparent' expand='lg'>
+                <Navbar.Brand className='navbar_left' href={''}>SETH MORENOS</Navbar.Brand>
+                <Navbar.Toggle className='mobile_nav' aria-controls='navbarNav'>
+                    <span>
+                        <>
+                        <div className='menu_bar'></div>
+                        <div className='menu_bar'></div>
+                        <div className='menu_bar'></div>
+                        </>
+                    </span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id='navbarNav'>
+                    <Nav className='nav_drop'>
+                        <Nav.Link className='nav_item' href={''}>About</Nav.Link>
+                        <Nav.Link className='nav_item' href={resumeFile}>Resume</Nav.Link>
+                        <Nav.Link className='nav_item' href={''}>LinkedIn</Nav.Link>
+                        <Nav.Link className='nav_item' href={''}>Behance</Nav.Link>
+                        <Nav.Link className='nav_item' href={''}>GitHub</Nav.Link>
+                        <Nav.Link className='nav_item' href={''}>Contact Me</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                <Nav className='desktop_nav' href={''}>ABOUT</Nav>
+            </Navbar>
         </div>
     );
 }
