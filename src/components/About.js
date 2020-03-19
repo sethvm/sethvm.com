140c9e0d6f6b8b354efc946c79ad2245bbe6cb0b
Seth Morenos <sethvm64@gmail.com>
2020-03-18 20:29:51 -0400

    update pages

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,9 @@ import Portrait from './about/about-banner.png';
 
 export default function About() {
     return(
-        <div id='wrapper' class='project animated fadeIn'>  
+        <div id='wrapper' class='project animated fadeIn'>
             <div class='proj_txt'>
-                <img class='proj_banner' src={Portrait} alt={'portrait'} />
+                <img class='proj_img' src={Portrait} alt={'portrait'} />
                 <p>
                     <br />
                 </p>
