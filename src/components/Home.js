6273bea670591afafb5157ad8283a9c685837fe1
Seth Morenos <sethvm64@gmail.com>
2020-03-23 11:47:26 -0400

    clean up styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,47 +9,47 @@ import './fonts/Futura_Demi.css';
 
 export default function Home() {
     return (
-        <div id='home'>
-            <div className='blue_block animated fadeInLeft'>
-                <>
-                    <div className='intro_sm'>
-                        <h1>DESIGNER</h1>
-                        <h6>SYSTEMS DESIGN ENGINEERING</h6>
-                        <h6>UNIVERSITY OF WATERLOO</h6>
-                    </div>
-                    <div className='intro_md'>
-                        <h1>DESIGNER</h1>
-                        <p>
-                            ENGINEERING STUDENT
-                            <br />UNIVERSITY OF WATERLOO
-                            <br />
-                            <br />Experienced in UI/UX Design
-                            <br />Interested in Front-end Development and Product Design
-                        </p>    
-                    </div>
-                </>
+        <>
+        <div className='blue_block animated fadeInLeft'>
+            <>
+            <div className='intro_sm'>
+                <h1>DESIGNER</h1>
+                <h6>SYSTEMS DESIGN ENGINEERING</h6>
+                <h6>UNIVERSITY OF WATERLOO</h6>
             </div>
-            <div className='intro_lg animated fadeIn'>
-                <h1>
-                    <strong>Hello! I'm Seth.</strong>
+            <div className='intro_md'>
+                <h1>DESIGNER</h1>
+                <p>
+                    ENGINEERING STUDENT
+                    <br />UNIVERSITY OF WATERLOO
                     <br />
-                </h1>
-                <p> 
-                    <br />Thanks for stopping by!
-                    <br />
-                    <br />I'm a <strong>designer</strong> pursuing a BASc. in
-                    <br /><strong>Systems Design Engineering</strong> with a
-                    <br />passion for creating products that leave
-                    <br />a positive impact in the world around me.
-                    <br />
-                    <br />Experienced in <strong>UI/UX</strong> with an
-                    <br />interest in <strong>front-end development</strong>
-                    <br />and <strong>product design</strong>.
-                    <br />
-                    <br />Previous position:
-                    <br />UX and Web Designer at Ontario Public Service.
-                </p>
+                    <br />Experienced in UI/UX Design
+                    <br />Interested in Front-end Development and Product Design
+                </p>    
             </div>
+            </>
         </div>
+        <div className='intro_lg animated fadeIn'>
+            <h1>
+                <strong>Hello! I'm Seth.</strong>
+                <br />
+            </h1>
+            <p> 
+                <br />Thanks for stopping by!
+                <br />
+                <br />I'm a <strong>designer</strong> pursuing a BASc. in
+                <br /><strong>Systems Design Engineering</strong> with a
+                <br />passion for creating products that leave
+                <br />a positive impact in the world around me.
+                <br />
+                <br />Experienced in <strong>UI/UX</strong> with an
+                <br />interest in <strong>front-end development</strong>
+                <br />and <strong>product design</strong>.
+                <br />
+                <br />Previous position:
+                <br />UX and Web Designer at Ontario Public Service.
+            </p>
+        </div>
+        </>
     );
 }
