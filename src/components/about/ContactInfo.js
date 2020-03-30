754d67bc6ceed75d9f85bc79646835dc52bd5700
Seth Morenos <sethvm64@gmail.com>
2020-03-30 00:21:10 -0400

    add contact link

--- src/components/about/ContactInfo.js
+++ src/components/about/ContactInfo.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import ResumeFile from '../pdf/sethvm_resume.pdf';
+
 import './ContactInfo.css';
 import '../fonts/Futura PT_Light.css';
 import '../fonts/Futura PT_Book.css';
@@ -9,6 +11,7 @@ export default function ContactInfo() {
     return (
         <div id='contact'>
             <Contact label='EMAIL' href='mailto:sethvm64@gmail.com?cc=svmoreno@uwaterloo.ca' /> |&nbsp;
+            <Contact label='RESUME' href={ResumeFile} /> |&nbsp;
             <Contact label='LINKEDIN' href='https://www.linkedin.com/in/sethvm' />
         </div>
     );
