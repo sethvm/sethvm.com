8113ff6494f7c754b0ae601c809c982335c656db
Seth Morenos <sethvm64@gmail.com>
2020-03-31 10:18:37 -0400

    clean up sidebar styling

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -11,18 +11,12 @@ import ContactLogo from './contact-logo.svg';
 
 export default function SideBar() {
     return (
-        <div className='sidebar'>
-            <div className='side_items animated fadeIn'>
-                <SideBarItem href={ResumeFile} img={ResumeLogo} alt='Resume' />
-                <p><br /></p>
-                <SideBarItem href='https://github.com/sethvm' img={GitHubLogo} alt='github.com/sethvm' />
-                <p><br /></p>
-                <SideBarItem href='https://www.behance.net/sethvm/' img={BehanceLogo} alt='behance.net/sethvm' />
-                <p><br /></p>
-                <SideBarItem href='https://www.linkedin.com/in/sethvm' img={LinkedInLogo} alt='linkedin.com/in/sethvm' />
-                <p><br /></p>
-                <SideBarItem href='mailto:sethvm64@gmail.com?cc=svmoreno@uwaterloo.ca' img={ContactLogo} alt='contact' />
-            </div>
+        <div className='sidebar animated fadeIn'>
+            <SideBarItem href={ResumeFile} img={ResumeLogo} alt='Resume' />
+            <SideBarItem href='https://github.com/sethvm' img={GitHubLogo} alt='github.com/sethvm' />
+            <SideBarItem href='https://www.behance.net/sethvm/' img={BehanceLogo} alt='behance.net/sethvm' />
+            <SideBarItem href='https://www.linkedin.com/in/sethvm' img={LinkedInLogo} alt='linkedin.com/in/sethvm' />
+            <SideBarItem href='mailto:sethvm64@gmail.com?cc=svmoreno@uwaterloo.ca' img={ContactLogo} alt='contact' />
         </div>
     );
 }
@@ -30,7 +24,7 @@ export default function SideBar() {
 function SideBarItem(props) {
     return (
         <a rel='noopener noreferrer' href={props.href} target='_blank'>
-            <img src={props.img} alt={props.alt} className='side_logo' />
+            <img src={props.img} alt={props.alt} className='item' />
         </a>
     );
 }
