b8e2c29c154da3adf0fac899bfd073c68fbcf3c9
Seth Morenos <sethvm64@gmail.com>
2020-04-08 01:30:23 -0400

    merge Page.js into index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    HashRouter,
+    Switch,
+    Route
+} from 'react-router-dom';
 
-import Page from './Page';
 import './index.css';
+import './components/animate.min.css';
+import './components/bootstrap.min.css';
+
+import HomeHeader from './components/Header/HomeHeader';
+import PageHeader from './components//Header/PageHeader';
+import Home from './components/Home';
+import About from './components/About';
+import Albert from './components/Albert';
+import FASSB from './components/FASSB';
+import Vector from './components/Vector';
+import BlueBlock from './components/BlueBlock/BlueBlock'
+import HomeCarousel from './components/Carousel/Carousel';
+import SideBar from './components/SideBar/SideBar';
+import TopShortcut from './components/TopShortcut/TopShortcut';
+import ScrollToTop from './components/TopShortcut/ScrollToTop';
+import HomeFooter from './components/Footer/HomeFooter';
+import PageFooter from './components/Footer/PageFooter';
+
+function Page() {
+    return (
+        <HashRouter>
+        <ScrollToTop />
+            <Switch>
+                <Route exact={true} path='/' component={HomePage} />
+                <Route exact={false} path='/' component={RegPage} />
+            </Switch>
+        </HashRouter>
+    );
+}
+
+function HomePage() {
+    return(
+        <>
+        <HomeHeader />
+        <Home />
+        <BlueBlock />
+        <HomeCarousel />
+        <SideBar />
+        <HomeFooter />
+        </>
+    );
+}
+
+function RegPage() {
+    return(
+        <>
+        <PageHeader />
+        <Switch>
+            <Route exact={true} path='/about' component={About} />
+            <Route exact={true} path='/albert' component={Albert} />
+            <Route exact={true} path='/fassb' component={FASSB} />
+            <Route exact={true} path='/vector' component={Vector} />
+        </Switch>
+        <SideBar />
+        <TopShortcut />
+        <PageFooter />
+        </>
+    );
+}
 
 ReactDOM.render(<Page />, document.getElementById('root'));
