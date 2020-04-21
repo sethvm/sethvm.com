1df8ee13a6b35cbb37a247e903048691c2495556
Seth Morenos <sethvm64@gmail.com>
2020-04-21 11:28:41 -0400

    code restructuring

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+    HashRouter,
+    Switch,
+    Route
+} from 'react-router-dom';
+
+import './components/animate.min.css';
+import './components/bootstrap.min.css';
+
+import HomeHeader from './components/Header/HomeHeader';
+import PageHeader from './components//Header/PageHeader';
+import Home from './components/Home';
+import About from './components/About';
+import Albert from './components/Albert';
+import FASSB from './components/FASSB';
+import Vector from './components/Vector';
+import BlueBlock from './components/BlueBlock/BlueBlock'
+import HomeCarousel from './components/Carousel/Carousel';
+import SideBar from './components/SideBar/SideBar';
+import TopShortcut from './components/TopShortcut/TopShortcut';
+import ScrollToTop from './components/TopShortcut/ScrollToTop';
+import HomeFooter from './components/Footer/HomeFooter';
+import PageFooter from './components/Footer/PageFooter';
+
+export default function App() {
+    return (
+        <HashRouter>
+        <ScrollToTop />
+            <Switch>
+                <Route exact={true} path='/' component={Landing} />
+                <Route exact={false} path='/' component={Page} />
+            </Switch>
+        </HashRouter>
+    );
+}
+
+function Landing() {
+    return(
+        <>
+        <HomeHeader />
+        <Home />
+        <BlueBlock />
+        <HomeCarousel />
+        <SideBar />
+        <HomeFooter />
+        </>
+    );
+}
+
+function Page() {
+    return(
+        <>
+        <PageHeader />
+        <Switch>
+            <Route exact={true} path='/about' component={About} />
+            <Route exact={true} path='/albert' component={Albert} />
+            <Route exact={true} path='/fassb' component={FASSB} />
+            <Route exact={true} path='/vector' component={Vector} />
+        </Switch>
+        <SideBar />
+        <TopShortcut />
+        <PageFooter />
+        </>
+    );
+}
--- src/components/TopShortcut/TopShortcut.js
+++ src/components/TopShortcut/TopShortcut.js
@@ -4,16 +4,14 @@ import BackToTop from 'react-back-to-top-button';
 import './TopShortcut.css';
 import UpArrowLogo from './up-arrow.svg';
 
-export default class TopShortcut extends React.Component {
-    render() {
-        return(
-            <BackToTop
-            showAt={800}
-            speed={1800}>
-                <div className='top_shortcut'>
-                    <img src={UpArrowLogo} className='shortcut_logo' alt='back to top' />
-                </div>
-            </BackToTop>
-        );
-    }
+export default function TopShortcut() {
+    return(
+        <BackToTop
+        showAt={800}
+        speed={1800}>
+            <div className='top_shortcut'>
+                <img src={UpArrowLogo} className='shortcut_logo' alt='back to top' />
+            </div>
+        </BackToTop>
+    );
 }
