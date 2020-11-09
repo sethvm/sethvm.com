855a81ec388345d0d55aed792a1ec150ff21f8e2
Seth Morenos <sethvm64@gmail.com>
2020-11-09 15:39:31 -0500

    update about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -19,22 +19,22 @@ export default function About() {
                 <p>
                     <br />
                     As a visual learner, the usage of illustrations and graphics to communicate has been a lifelong passion of mine.
-                    I am drawn toward the fields of design and development because they allow me to extend this passion towards the
-                    creation of purposeful products that encompass a user's needs beyond its visual aspects.
+                    I am drawn toward the fields of design and development because they allow me to extend this passion towards learning
+                    about the people for which products and experiences are created.
                     <br />
                     <br />
-                    Currently in the second year of my undergraduate studies at the University of Waterloo, I am majoring in
+                    Currently an undergraduate student at the University of Waterloo, I am majoring in
                     the field of Systems Design Engineering (SYDE). <abbr title='Systems Design Engineering'>SYDE</abbr> is a 
                     unique department that employs the integration of knowledge across multiple Engineering disciplines.
                     I seek to develop my ability to tackle complex problems through my passion and education in order to 
                     drive positive change within the society around me.
                     <br />
                     <br />
-                    Outside of class and work, I spend most of my time in the bouldering gym or the weight room. Occasionally, I'll boot
-                    up a racing sim and put in a few flying laps.
+                    Outside of class and work, I spend most of my time in the weight room or learning to cook a new dish. Occasionally,
+                    I enjoy jumping out of flying aircraft - with a parachute, of course.
                     <br />
                     <br />
-                    Feel free to reach out! I'm always up for a chat about new ideas or opportunities!
+                    If you've made it this far into my introduction, don't hesitate to get in touch!
                     <br />
                     <br />
                     <ProfileLink label='LINKEDIN' href='https://www.linkedin.com/in/sethvm' />&nbsp;&nbsp;&nbsp;
