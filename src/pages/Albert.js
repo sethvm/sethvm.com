628a16bf670df5ce3b915c50d1113c772d896266
Seth Morenos <sethvm64@gmail.com>
2020-11-09 03:36:59 -0500

    project albert rework

--- src/pages/Albert.js
+++ src/pages/Albert.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../styles/Project.css';
 
-import Banner from '../components/albert/banner_albert.svg';
-import Ideation from '../components/albert/logo-ideation_albert.png';
-import PlainLogo from '../components/albert/plain-logo_albert.svg';
-import ColourGuide from '../components/albert/colour-guide_albert.svg';
+import Banner from '../components/albert/albert-banner.svg';
+import ColourGuide from '../components/albert/albert-colour_guide.svg';
+import ColouredLogo from '../components/albert/albert-coloured_logo.svg';
+import PlainLogo from '../components/albert/albert-plain_logo.svg';
 
 export default function Albert() {
     
@@ -24,9 +24,7 @@ export default function Albert() {
                 </p>
                 <h4><strong>TEAM</strong></h4>
                 <p>
-                    <strong>Seth Morenos</strong> - Designer
-                    <br />
-                    <strong>Jonathan Ho</strong> - Developer
+                    This is an independent project consisting of a developer and I (designer).
                     <br />
                     <br />
                 </p>
@@ -44,35 +42,6 @@ export default function Albert() {
                     <br />
                     <br />
                 </p>
-                <h4><strong>USER RESEARCH</strong></h4>
-                <p>
-                    To better understand the audience we are targeting with Project Albert, we conducted a survey 
-                    to obtain insight into what factors are considered prior to deciding upon renting or
-                    purchasing a place. Our respondents consisted of University of Waterloo co-op students (who
-                    are going into their co-op terms and are searching for places to live) and newcomers to the
-                    Greater Toronto Area (GTA).
-                    <br />
-                    <br/>
-                    Combining the results from this survey with information gathered through secondary research involving
-                    newcomers, we came across the following key findings:
-                    <br />
-                    <br />
-                    <div className='indented'>
-                        <strong>1.</strong> The vast majority of newcomers and students alike do not have access to personal transport,
-                        and thus value proximity to public transit - respondents both demographics have even expressed willingness to forgo proximity
-                        to work for the sake of being close to public transit.
-                        <br />
-                        <br />
-                        <strong>2.</strong> Proximity to shopping centers is also of importance, albeit to a lesser degree in comparison
-                        to public transit. Newcomers placed value on proximity to tourist attractions, while students gravitated towards
-                        services such as fitness centers and hospitals.
-                        <br />
-                        <br />
-                        <strong>3.</strong> Because newcomers and students are often running with tight budgets, renting/housing cost
-                        is the most heavily weighed non-geographical factor when deciding on a place to live.
-                    </div>
-                    <br />
-                </p>
                 <h4><strong>BRANDING</strong></h4>
                 <p>
                     With every product comes a branding identity that enables it to stand out.
@@ -81,21 +50,24 @@ export default function Albert() {
                     Because Project Albert is an application that provides assistance with finding residence options,
                     I wanted to create a logo that symbolizes housing combined with some form of mapping-related visual.
                     Furthermore, I wanted this logo to be flat and easily recognizable. Our logo design takes inspiration
-                    from a house highlighted with a map marker, resembling the letters A and P respectively.
-                    <br />
-                    <img className='project_img' src={Ideation} alt='logo ideation' />
+                    from a house highlighted with a map marker, resembling the letters A and P, respectively.
                     <br />
                     <br />
-                    Fine-tuning the gap and notch on the letter A allowed me to create structures resembling a window 
-                    and a door. After making more design changes to balance out the volume filled by each 
-                    shape, the finalized logo (below) is eventually finished.
-                    <br />
+                    <div className='project_img_caption'>
+                        <p>                  
+                            <em><strong>Project Logo</strong></em>
+                            <br/>
+                        </p>
+                    </div> {/* /caption */}
                     <img className='project_img' src={PlainLogo} alt='plain logo' />
+                    <img className='project_img' src={ColouredLogo} alt='coloured logo' />
                     <br />
-                    The colour palettes have been chosen
-                    following Google's Material Design guidelines, coming in both dark and light variants. The shades
-                    of blue represent the relief and calm that newcomers feel upon locating or securing a new home, while
-                    the red and orange embody the warmth and security provided by shelter.
+                    <br />
+                    <br />
+                    Our branding colour palette was created following Google's Material design guidelines. Blue represents the
+                    relief and calm felt by prospective residents upon securing a new home, while orange embodies the warmth and
+                    security provided by shelter. A background colour symbolizes the uncertainty encountered upon searching for
+                    a new place to live.
                     <br />
                     <br />
                     <div className='project_img_caption'>
@@ -105,6 +77,36 @@ export default function Albert() {
                         </p>
                     </div> {/* /caption */}
                     <img className='project_img' src={ColourGuide} alt='colour guide' />
+                    <br />
+                    <br />
+                </p>
+                <h4><strong>USER RESEARCH</strong></h4>
+                <p>
+                    To better understand the audience we are targeting with Project Albert, we conducted a survey 
+                    to obtain insight into what factors are considered prior to deciding upon renting or
+                    purchasing a place. Our respondents consisted of University of Waterloo co-op <strong>students</strong> (who
+                    are going into their co-op terms and are searching for places to live) and <strong>newcomers</strong> to the
+                    Greater Toronto Area (GTA).
+                    <br />
+                    <br/>
+                    Combining the results from this survey with information gathered through secondary research involving
+                    newcomers, we came across the following key findings:
+                    <br />
+                    <br />
+                    <div className='indented'>
+                        <strong>1)</strong> The vast majority of newcomers and students alike do not have access to personal transport,
+                        and thus value proximity to public transit - respondents both demographics have even expressed willingness to forgo proximity
+                        to work for the sake of being close to public transit.
+                        <br />
+                        <br />
+                        <strong>2)</strong> Proximity to shopping centers is also of importance, albeit to a lesser degree in comparison
+                        to public transit. Newcomers placed value on proximity to tourist attractions, while students gravitated towards
+                        services such as fitness centers and hospitals.
+                        <br />
+                        <br />
+                        <strong>3)</strong> Because newcomers and students are often running with tight budgets, renting/housing cost
+                        is the most heavily weighed non-geographical factor when deciding on a place to live.
+                    </div>
                     <br />
                     <br />
                 </p>
