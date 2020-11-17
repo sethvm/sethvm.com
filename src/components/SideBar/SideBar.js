a98d137bcfabcf79c462fa2d5a6d70e493f68991
Seth Morenos <sethvm64@gmail.com>
2020-11-17 13:45:43 -0500

    updates: preview, sidebar and resume

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './SideBar.css';
 
+import BehanceLogo from './behance-logo.svg';
+import ContactLogo from './contact-logo.svg';
+import GitHubLogo from './github-logo.svg';
+import LinkedInLogo from './linkedin-logo.svg';
 import ResumeFile from '../../pdf/sethvm_resume.pdf';
 import ResumeLogo from './resume-logo.svg';
-import LinkedInLogo from './linkedin-logo.svg';
-import FigmaLogo from './figma-logo.svg';
-import GitHubLogo from './github-logo.svg';
-import ContactLogo from './contact-logo.svg';
 
 export default function SideBar() {
     return (
         <div className='sidebar animated fadeIn'>
             <SideBarItem href={ResumeFile} img={ResumeLogo} alt='resume' />
+            <SideBarItem href='https://www.behance.net/sethvm/' img={BehanceLogo} alt='behance.net/sethvm' />
             <SideBarItem href='https://github.com/sethvm' img={GitHubLogo} alt='github.com/sethvm' />
-            <SideBarItem href='https://www.figma.com/@sethvm' img={FigmaLogo} alt='figma.com/@sethvm' />
             <SideBarItem href='https://www.linkedin.com/in/sethvm' img={LinkedInLogo} alt='linkedin.com/in/sethvm' />
             <SideBarItem href='mailto:sethvm64@gmail.com?cc=svmoreno@uwaterloo.ca' img={ContactLogo} alt='contact' />
         </div>
