37e056b9f9f29c1f9bafecdc732ad2b61b9611b1
Seth Morenos <sethvm64@gmail.com>
2020-11-22 16:36:51 -0500

    update about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import '../styles/Project.css';
 
-import Portrait from '../components/about/about-banner.png';
+import Gondola from '../components/about/gondola.png';
 import ProfileLink from '../components/about/ProfileLink';
 import ResumeFile from '../pdf/sethvm_resume.pdf';
+import SkyDive from '../components/about/skydive.png';
 
 export default function About() {
     
@@ -11,15 +12,11 @@ export default function About() {
         <>
         <main className='project animated fadeIn'>
             <div className='project_body'>
-                <img className='project_img' src={Portrait} alt='self portrait' />
-                <p>
-                    <br />
-                </p>
-                <h4><strong>DESIGNER, ENGINEERING STUDENT</strong></h4>
+                <h4><strong>DESIGNER - ENGINEERING STUDENT - ADRENALINE JUNKIE</strong></h4>
                 <p>
                     <br />
                     As a visual learner, the usage of illustrations and graphics to communicate has been a lifelong passion of mine.
-                    I am drawn toward the fields of design and development by the opportunity to extend this passion towards creating
+                    I am drawn toward the fields of design and development by the opportunity to extend this passion towards crafting
                     products and experiences that make life easier for others.
                     <br />
                     <br />
@@ -30,8 +27,8 @@ export default function About() {
                     drive positive change within the society around me.
                     <br />
                     <br />
-                    Outside of class and work, I spend most of my time in the weight room or learning to cook a new dish. Occasionally,
-                    I enjoy jumping out of flying aircraft - with a parachute, of course.
+                    Outside of design and school, I spend most of my time in the bouldering gym, standing in line to board a roller coaster
+                    or learning to cook a new dish. Occasionally, I enjoy jumping out of flying aircraft - with a parachute, of course.
                     <br />
                     <br />
                     Made it this far into my introduction? Don't hesitate to get in touch!
@@ -40,8 +37,17 @@ export default function About() {
                     <ProfileLink label='LINKEDIN' href='https://www.linkedin.com/in/sethvm' />&nbsp;&nbsp;&nbsp;
                     <ProfileLink label='EMAIL' href='mailto:sethvm64@gmail.com?cc=svmoreno@uwaterloo.ca' />&nbsp;&nbsp;&nbsp;
                     <ProfileLink label='RESUME' href={ResumeFile} />
+                    <br />
+                    <br />
+                    <hr />
                 </p>
             </div> {/* /project_body */}
+            <div className='img-fluid'>
+                <img className='img-fluid_card' src={Gondola} alt='Formula 1 (Figma)' />
+                <p><br /></p>
+                <img className='img-fluid_card' src={SkyDive} alt='I sometimes skydive' />
+                <p><br /></p>
+            </div> {/* /img-fluid */}
         </main> {/* /project */}
         </>
     );
