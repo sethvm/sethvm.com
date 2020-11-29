17c36962bf6f0126619005f3deb2031075b54bc4
Seth Morenos <sethvm64@gmail.com>
2020-11-28 22:36:46 -0500

    rebuild cards with flexbox

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -17,42 +17,42 @@ export default function Projects() {
                 <p>
                     Work experience, passion projects and some occasional scribbles
                 </p>
+                <Card
+                link='/soulfx'
+                img={SoulfxCard}
+                alt='Soulfx patient support application'
+                title='PATIENT SUPPORT APP'
+                desc='Bringing remote services to patients in need'>
+                    <Badge pill variant='light'>Software / UX Design</Badge>&nbsp;
+                    <Badge pill variant='light'>Information Architecture</Badge>
+                </Card>
+                <Card
+                link='/albert'
+                img={AlbertCard}
+                alt='Project ALbert'
+                title='PROJECT ALBERT'
+                desc='Mitigating the stress of relocating'>
+                    <Badge pill variant='light'>Branding</Badge>&nbsp;
+                    <Badge pill variant='light'>UI / UX Design</Badge>
+                </Card>
+                <Card
+                link='/fassb'
+                img={FASSBCard}
+                alt='FASSB UI/UX'
+                title='FASSB UI/UX'
+                desc='My first design internship in a nutshell'>
+                    <Badge pill variant='light'>UI / UX Design</Badge>&nbsp;
+                    <Badge pill variant='light'>Data Visualization</Badge>
+                </Card>
+                <Card
+                link='/visuals'
+                img={VisualsCard}
+                alt='Visuals'
+                title='VISUALS'
+                desc='A collection of miscellaneous designs'>
+                    <Badge pill variant='light'>Graphic Design</Badge>&nbsp;
+                </Card>
             </div>
-            <Card
-            link='/soulfx'
-            img={SoulfxCard}
-            alt='Soulfx patient support application'
-            title='PATIENT SUPPORT APP'
-            desc='Bringing remote services to patients in need'>
-                <Badge pill variant='light'>Software / UX Design</Badge>&nbsp;
-                <Badge pill variant='light'>Information Architecture</Badge>
-            </Card>
-            <Card
-            link='/albert'
-            img={AlbertCard}
-            alt='Project ALbert'
-            title='PROJECT ALBERT'
-            desc='Mitigating the stress of relocating'>
-                <Badge pill variant='light'>Branding</Badge>&nbsp;
-                <Badge pill variant='light'>UI / UX Design</Badge>
-            </Card>
-            <Card
-            link='/fassb'
-            img={FASSBCard}
-            alt='FASSB UI/UX'
-            title='FASSB UI/UX'
-            desc='My first design internship in a nutshell'>
-                <Badge pill variant='light'>UI / UX Design</Badge>&nbsp;
-                <Badge pill variant='light'>Data Visualization</Badge>
-            </Card>
-            <Card
-            link='/visuals'
-            img={VisualsCard}
-            alt='Visuals'
-            title='VISUALS'
-            desc='A collection of miscellaneous designs'>
-                <Badge pill variant='light'>Graphic Design</Badge>&nbsp;
-            </Card>
         </main>
     );
 }
@@ -64,13 +64,10 @@ function Card(props) {
             <br />
         </p>
         <Link to={props.link} className='card_link'>
-            <div className='img-fluid project_card'>
-                <img className='img-fluid_card' src={props.img} alt={props.alt} />
-                <div className='img-fluid_card card_text'>
-                    <p>
-                        <br />
-                    </p>
-                        <h5><strong>{props.title}</strong></h5>
+            <div className='project_card'>
+                <img className='card_img' src={props.img} alt={props.alt} />
+                <div className='card_text'>
+                    <h5><strong>{props.title}</strong></h5>
                     <p>
                         {props.desc}
                         <br />
