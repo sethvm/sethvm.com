a9b82af892659d3e1050394c1dadb7b59ab12d78
Seth Morenos <sethvm64@gmail.com>
2021-04-08 19:07:58 -0400

    configure router for hosting change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import {
     lazy
 } from 'react';
 import {
-    HashRouter,
+    BrowserRouter,
     Switch,
     Route
 } from 'react-router-dom';
@@ -34,7 +34,7 @@ const Loblaw = lazy(() => import('./pages/Loblaw'));
 
 export default function App() {
     return (
-        <HashRouter>
+        <BrowserRouter>
             <TrackPageViews />
             <ScrollToTop />
             <BlueBlock />
@@ -65,6 +65,6 @@ export default function App() {
             </Container>
             <SideBar />
             <Footer />
-        </HashRouter>
+        </BrowserRouter>
     );
 }
